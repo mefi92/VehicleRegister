33054ec37e031dd35f403d1804b6cf66e4517da6
mefi92 <sgrcsont@gmail.com>
2024-01-19 08:55:09 +0100

    Linked Core and Vehicle projects.

--- Core/PersistentVehicleGateway.cs
+++ Core/PersistentVehicleGateway.cs
@@ -4,11 +4,15 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entity;
+ 
 
 namespace Core
 {
     internal interface PersistentVehicleGateway
     {
-        
+        Vehicle loadVehicle(string carRegistrationNumber);
+        void saveVehicle(Vehicle vehicle);
+         
     }
 }
--- Entity/Vehicle.cs
+++ Entity/Vehicle.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entity
 {
-    internal class Vehicle
+    public class Vehicle
     {
         public string type;
         public string registrationNumber; 

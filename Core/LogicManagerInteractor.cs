219592fd01e82ca3663cfd3008dea21513df3df1
mefi92 <sgrcsont@gmail.com>
2024-01-19 12:13:57 +0100

    registerNewVehicle method added

--- Core/LogicManagerInteractor.cs
+++ Core/LogicManagerInteractor.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entity;
 
 namespace Core
 {
     public class LogicManagerInteractor : VehicleManagerInBoundary
     {
         private PersistentVehicleGateway persistentVehicleGateway;
-        private VehicleManagerPresenterOutBoundary presenterOutBoundary;
+        private VehicleManagerPresenterOutBoundary presenterManager;
 
-        public LogicManagerInteractor(PersistentVehicleGateway persistentVehicleGateway, VehicleManagerPresenterOutBoundary presenterOutBoundary)
+        public LogicManagerInteractor(PersistentVehicleGateway persistentVehicleGateway, VehicleManagerPresenterOutBoundary presenterManager)
         {
             this.persistentVehicleGateway = persistentVehicleGateway;
 
-            this.presenterOutBoundary = presenterOutBoundary;
+            this.presenterManager = presenterManager;
         }
 
         public void registerNewVehicle(string vehicleType, string engineNumber)
         {
-            
+            Vehicle newVehicle = new RegisterNewVehicle().addNewVehicle(vehicleType, engineNumber);
+            persistentVehicleGateway.saveVehicle(newVehicle);
+            presenterManager.displayMessage("Car is registered with plate number:" + newVehicle.registrationNumber);           
+
         }
     }
 }

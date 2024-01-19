e4b48d004f898c480921ecc0f7f85889800cb9e0
mefi92 <sgrcsont@gmail.com>
2024-01-19 10:05:21 +0100

    FakeUi and ManagerPresenterOutBoundary had added.

---
+++ Core/LogicManagerInteractor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class LogicManagerInteractor : VehicleManagerInBoundary
+    {
+        private PersistentVehicleGateway persistentVehicleGateway;
+        private
+
+
+        public LogicManagerInteractor()
+        {
+            
+        }
+
+        public void registerNewVehicle(string carType, string engineNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

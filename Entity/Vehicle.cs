15b8de0d66f4e0c8a839405f2f45e5a5cb7ada30
mefi92 <sgrcsont@gmail.com>
2024-01-19 08:33:43 +0100

    Project Core and Entity creation.

---
+++ Entity/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    internal class Vehicle
+    {
+        public string type;
+        public string registrationNumber; 
+        public string engineNumber;
+
+        public Vehicle(string vehicleType, string vehicleRegistrationNumber, string vehicleEngineNumber)
+        {
+            type = vehicleType;
+            registrationNumber = vehicleRegistrationNumber;
+            engineNumber = vehicleEngineNumber;
+        }
+
+    }
+}

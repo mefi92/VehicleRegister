ebde79f0600ac1e3802f49e917610bddec18b99d
mefi92 <sgrcsont@gmail.com>
2024-01-19 15:19:02 +0100

    Write / Read text.

--- Persistence/VehicleFileSystem.cs
+++ Persistence/VehicleFileSystem.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core;
+using Entity;
+using System.IO.Enumeration;
 
 namespace Persistence
 {
-    internal class VehicleFileSystem
+    internal class VehicleFileSystem : PersistentVehicleGateway
     {
+        private string fileType = ".txt";
+
+        public Vehicle loadVehicle(string carRegistrationNumber)
+        {
+            string fileName = carRegistrationNumber + fileType;
+            try
+            {
+                FileStream stream = File.Open(fileName, FileMode.Open);
+                StreamReader reader = new StreamReader(stream);
+
+
+                string vehicleType = reader.ReadLine().Trim();
+                string engineNumber = reader.ReadLine().Trim();
+                int registrationNumber = int.Parse(reader.ReadLine().Trim());
+                reader.Close();
+
+                Vehicle vehicle = new Vehicle(vehicleType, registrationNumber, engineNumber);
+                return vehicle;
+            }
+            catch 
+            {
+                return null;
+            }
+        }
+
+        public void saveVehicle(Vehicle vehicle)
+        {
+            string fileName = vehicle.registrationNumber + fileType;
+
+            try
+            {
+                FileStream stream = File.Open(fileName, FileMode.Create);
+                StreamWriter writer = new StreamWriter(stream);
+                writer.Write(vehicle.type);
+                writer.WriteLine(true);
+                writer.Write(vehicle.engineNumber);
+                writer.WriteLine(true);
+                writer.Write(vehicle.registrationNumber.ToString());
+                writer.Close();
+            }
+            catch
+            {
+                throw new Exception();
+            }
+            
+        }
+                
     }
 }
